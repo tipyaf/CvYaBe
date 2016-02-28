8a18d9464ba756d6fd59a78b62faf7b52f7533e3
tipyaf <ya.net@hotmail.fr>
2016-02-28 04:31:21 +0100

    change modal encours

--- js/cvVideo.js
+++ js/cvVideo.js
@@ -1,16 +1,35 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+//
+//   $("#vidButton").animatedModal(
+//     {
+//     animatedIn:'slideInLeft',
+//     animatedOut:'fadeOutLeft',
+//     color:'#000000'
+//   }
+// );
+//
+// });
 
-  $("#vidButton").animatedModal(
-    {
-    animatedIn:'slideInLeft',
-    animatedOut:'fadeOutLeft',
-    color:'#000000'
-  }
-);
 
-});
 
- $('.closeCv').on('click', function() {
+var cvVidButton = document.getElementById('vidButton');
+var cvVideo = document.getElementById('cvVideo');
+var closeCvVideo = document.querySelector('h1');
+cvVideo.style.display = "none";
+
+function openModal(){
+cvVideo.style.display = "block";
+}
+
+function closeModal(){
+  cvVideo.style.display = "none";
+}
+
+
+cvVidButton.addEventListener('click', openModal, false);
+closeCvVideo.addEventListener('click', closeModal, false);
+
+ $('h1').on('click', function() {
 
     $("#cvVideo")[0].src += "?autoplay=0";
         ev.preventDefault();
@@ -18,11 +37,14 @@ $(document).ready(function() {
 
 
 // lecture youtube à l'ouverture.
-// $(document).ready(function() {
-//   $('#vidButton').on('click', function(ev) {
-//
-//     $("#cvVideo")[0].src += "&autoplay=1";
-//     ev.preventDefault();
-//
-//   });
-// });
+$(document).ready(function() {
+  $('#vidButton').on('click', function(ev) {
+
+    $("#cvVideo")[0].src += "&autoplay=1";
+    ev.preventDefault();
+
+    $('html,body').animate({
+        scrollTop: $("#cvVideo").offset().top},
+        'slow');
+  });
+});
--- js/scroll.js
+++ js/scroll.js
@@ -1,35 +1,40 @@
-var body = document.querySelector('body');
-
-if (body.addEventListener) body.addEventListener('DOMMouseScroll', wheel, false);
-body.onmousewheel = document.onmousewheel = wheel;
-
-function wheel(event) {
-    var delta = 0;
-    if (event.wheelDelta) delta = event.wheelDelta / 120;
-    else if (event.detail) delta = -event.detail / 30;
-
-    handle(delta);
-    if (event.preventDefault) event.preventDefault();
-    event.returnValue = false;
-}
-
-function handle(delta) {
-    var time = 100;
-	var distance = 500;
-
-    $('html, body').stop().animate({
-        scrollTop: $(window).scrollTop() - (distance * delta)
-    }, time );
-}
-
+// var body = document.querySelector('body');
+//
+// if (body.addEventListener) body.addEventListener('DOMMouseScroll', wheel, false);
+// body.onmousewheel = document.onmousewheel = wheel;
+//
+// function wheel(event) {
+//     var delta = 0;
+//     if (event.wheelDelta) delta = event.wheelDelta / 120;
+//     else if (event.detail) delta = -event.detail / 30;
+//
+//     handle(delta);
+//     if (event.preventDefault) event.preventDefault();
+//     event.returnValue = false;
+// }
+//
+// function handle(delta) {
+//     var time = 100;
+// 	var distance = 500;
+//
+//     $('html, body').stop().animate({
+//         scrollTop: $(window).scrollTop() - (distance * delta)
+//     }, time );
+// }
+//
 // function downScroll() {
 //     window.scrollBy(0, 750);
 //
 // }
+
+
+
+
 $(".tosavoirs").click(function() {
     $('html,body').animate({
         scrollTop: $("#savoirs").offset().top},
         'slow');
+
 });
 $(".toblockquote").click(function() {
     $('html,body').animate({
