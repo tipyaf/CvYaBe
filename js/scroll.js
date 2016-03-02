e8ff6595e7dba5a18a00dbbecc26d38065070b7e
tipyaf <ya.net@hotmail.fr>
2016-03-03 00:04:27 +0100

    savoirs presque fini

--- js/scroll.js
+++ js/scroll.js
@@ -1,31 +1,31 @@
-var body = document.querySelector('body');
-
-if (body.addEventListener) body.addEventListener('DOMMouseScroll', wheel, false);
-body.onmousewheel = document.onmousewheel = wheel;
-
-function wheel(event) {
-    var delta = 0;
-    if (event.wheelDelta) delta = event.wheelDelta / 120;
-    else if (event.detail) delta = -event.detail / 30;
-
-    handle(delta);
-    if (event.preventDefault) event.preventDefault();
-    event.returnValue = false;
-}
-
-function handle(delta) {
-    var time = 500;
-	var distance = 400;
-
-    $('html, body').stop().animate({
-        scrollTop: $(window).scrollTop() - (distance * delta)
-    }, time );
-}
-
-function downScroll() {
-    window.scrollBy(0, 750);
-
-}
+// var body = document.querySelector('body');
+//
+// if (body.addEventListener) body.addEventListener('DOMMouseScroll', wheel, false);
+// body.onmousewheel = document.onmousewheel = wheel;
+//
+// function wheel(event) {
+//     var delta = 0;
+//     if (event.wheelDelta) delta = event.wheelDelta / 120;
+//     else if (event.detail) delta = -event.detail / 30;
+//
+//     handle(delta);
+//     if (event.preventDefault) event.preventDefault();
+//     event.returnValue = false;
+// }
+//
+// function handle(delta) {
+//     var time = 500;
+// 	var distance = 400;
+//
+//     $('html, body').stop().animate({
+//         scrollTop: $(window).scrollTop() - (distance * delta)
+//     }, time );
+// }
+//
+// function downScroll() {
+//     window.scrollBy(0, 750);
+//
+// }
 
 
 
