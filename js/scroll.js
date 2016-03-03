0270b7f1ba6063ce6b46cdb3163918717d1d39b9
tipyaf <ya.net@hotmail.fr>
2016-03-03 20:53:19 +0100

    parties finies

--- js/scroll.js
+++ js/scroll.js
@@ -1,38 +1,38 @@
-// var body = document.querySelector('body');
-//
-// if (body.addEventListener) body.addEventListener('DOMMouseScroll', wheel, false);
-// body.onmousewheel = document.onmousewheel = wheel;
-//
-// function wheel(event) {
-//     var delta = 0;
-//     if (event.wheelDelta) delta = event.wheelDelta / 120;
-//     else if (event.detail) delta = -event.detail / 30;
-//
-//     handle(delta);
-//     if (event.preventDefault) event.preventDefault();
-//     event.returnValue = false;
-// }
-//
-// function handle(delta) {
-//     var time = 500;
-// 	var distance = 400;
-//
-//     $('html, body').stop().animate({
-//         scrollTop: $(window).scrollTop() - (distance * delta)
-//     }, time );
-// }
-//
-// function downScroll() {
-//     window.scrollBy(0, 750);
-//
-// }
+var body = document.querySelector('body');
+
+if (body.addEventListener) body.addEventListener('DOMMouseScroll', wheel, false);
+body.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+    var delta = 0;
+    if (event.wheelDelta) delta = event.wheelDelta / 120;
+    else if (event.detail) delta = -event.detail / 30;
+
+    handle(delta);
+    if (event.preventDefault) event.preventDefault();
+    event.returnValue = false;
+}
+
+function handle(delta) {
+    var time = 500;
+	var distance = 400;
+
+    $('html, body').stop().animate({
+        scrollTop: $(window).scrollTop() - (distance * delta)
+    }, time );
+}
+
+function downScroll() {
+    window.scrollBy(0, 750);
+
+}
 
 
 
 
 $(".tosavoirs").click(function() {
     $('html,body').animate({
-        scrollTop: $("#savoirs").offset().top},
+        scrollTop: $("#parcours").offset().top},
         'slow');
 
 });
@@ -43,12 +43,12 @@ $(".toblockquote").click(function() {
 });
 $(".toparcours").click(function() {
     $('html,body').animate({
-        scrollTop: $("#parcours").offset().top},
+        scrollTop: $("#savoir-etre").offset().top},
         'slow');
 });
 $(".toetre").click(function() {
     $('html,body').animate({
-        scrollTop: $("#savoir-etre").offset().top},
+        scrollTop: $("#savoirs").offset().top},
         'slow');
 });
 $(".tocontacts").click(function() {
@@ -57,11 +57,11 @@ $(".tocontacts").click(function() {
         'slow');
 });
 
-$(document).ready(function() {
-
-  // $('.home .parallax-window').parallax({imageSrc: '../images/macback.jpg'});
-  $('.savoirs .parallax-window').parallax({imageSrc: '../images/knowback.jpg'});
-  $('.parcours .parallax-window').parallax({imageSrc: '../images/bodyback.jpg'});
-  $('savoir-etre .parallax-window').parallax({imageSrc: '../images/myback.jpg'});
-  // $('.article').parallax({imageSrc: '../images/homeback.jpg'});
-});
+// $(document).ready(function() {
+//
+//   // $('.home .parallax-window').parallax({imageSrc: '../images/macback.jpg'});
+//   $('.savoirs .parallax-window').parallax({imageSrc: '../images/archibackcolor.jpg'});
+//   $('.parcours .parallax-window').parallax({imageSrc: '../images/bodyback.jpg'});
+//   $('savoir-etre .parallax-window').parallax({imageSrc: '../images/myback.jpg'});
+//   // $('.article').parallax({imageSrc: '../images/homeback.jpg'});
+// });
